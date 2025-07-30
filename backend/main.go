5fa62722b426338f44908bba2a4cc45370bfd3c8
jordan-julio <jordan.julio.jap@gmail.com>
2025-07-30 10:46:18 +0700

    Re-added frontend after removing nested git

---
+++ backend/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"github.com/jordan-julio/project/backend/config"
+	"github.com/jordan-julio/project/backend/routes"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config.ConnectDB()
+
+	r := gin.Default()
+	routes.RegisterRoutes(r)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	r.Run(":" + port)
+}
